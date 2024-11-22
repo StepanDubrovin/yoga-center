2c9677856a7d52e02737f2cc094d7b0a2d78f12e
Stepa <stepan.09myromec@gmail.com>
2024-11-22 18:26:32 +0500

    fixTable

--- frontend/src/components/TableSchedule/TableSchedule.tsx
+++ frontend/src/components/TableSchedule/TableSchedule.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Box, Typography } from '@mui/material';
+import { Box, Typography, Grid } from '@mui/material';
 import { useSelector } from 'react-redux';
-import { RootState } from '../../reducer/store'
+import { RootState } from '../../reducer/store';
+import YogaIcon from '@mui/icons-material/SelfImprovement'; // Используем иконки (замени на нужные)
+
+
 
 interface ClassDetails {
   time: string;
@@ -9,52 +12,52 @@ interface ClassDetails {
 }
 
 const ScheduleTable: React.FC = () => {
- 
   const schedule = useSelector((state: RootState) => state.schedule.schedule);
 
+  // Иконки для дней недели
+  const dayIcons = {
+    Понедельник: <YogaIcon fontSize="large" />,
+    Вторник: <YogaIcon fontSize="large" />,
+    Среда: <YogaIcon fontSize="large" />,
+    Четверг: <YogaIcon fontSize="large" />,
+    Пятница: <YogaIcon fontSize="large" />,
+    Суббота: <YogaIcon fontSize="large" />,
+  };
+
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650, marginTop: '70px', }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            {Object.keys(schedule).map(day => (
-              <TableCell key={day} align="center">
-                <Typography variant="h6">{day}</Typography>
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            {Object.keys(schedule).map(day => (
-              <TableCell key={day} align="center" sx={{ verticalAlign: 'top', paddingRight: '25px' }}>
-                {schedule[day].map((classDetail: ClassDetails, index: number) => (
-                  <Box
-                    key={index}
-                    sx={{
-                      backgroundColor: '#E6E6FA',
-                      color: 'black',
-                      padding: '25px',
-                      borderRadius: '10px',
-                      marginBottom: '50px',
-                      width: '200px',
-                      display: 'flex',
-                      flexDirection: 'column',
-                      justifyContent: 'center',
-                      alignItems: 'center',
-                      marginLeft: '30px',
-                    }}
-                  >
-                    <Typography>{classDetail.time}</Typography>
-                    <Typography>{classDetail.type}</Typography>
-                  </Box>
-                ))}
-              </TableCell>
-            ))}
-          </TableRow>
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <Box sx={{ textAlign: 'center', marginTop: '80px' }}>
+      
+      <Grid container spacing={5} justifyContent="center">
+        {Object.keys(schedule).map((day) => (
+          <Grid item key={day} xs={12} sm={6} md={4} lg={3}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+              {dayIcons[day as keyof typeof dayIcons]} {/* Иконка для дня */}
+              <Typography variant="h6" sx={{ fontWeight: 'bold', margin: '10px 0' }}>
+                {day}
+              </Typography>
+              {schedule[day].map((classDetail: ClassDetails, index: number) => (
+                <Box
+                  key={index}
+                  sx={{
+                    backgroundColor: '#E6E6FA',
+                    padding: '10px',
+                    borderRadius: '10px',
+                    marginBottom: '15px',
+                    width: '200px',
+                    textAlign: 'center',
+                  }}
+                >
+                  <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+                    {classDetail.time}
+                  </Typography>
+                  <Typography variant="body2">{classDetail.type}</Typography>
+                </Box>
+              ))}
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 };
 
